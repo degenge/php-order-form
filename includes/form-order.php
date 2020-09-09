498ca767c8b1125c6c271e3a80cc0c61d2f48f11
Gerrit Degenhardt <gerrit@degenhardt.be>
2020-09-09 16:21:56 +0200

    Setup form

--- includes/form-order.php
+++ includes/form-order.php
@@ -1,50 +1,46 @@
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" class="w-full max-w-lg" >
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="w-full max-w-lg" >
 
     <div class="flex flex-wrap -mx-3 mb-6" >
 
         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0" >
             <label for="name-first" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >First Name</label >
-            <input type="text" id="name-first" name="name-first"
-                   class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" >
-            <p class="text-red-500 text-xs italic" >Please fill out this field.</p >
+            <input type="text" id="name-first" name="name-first" value="<?php echo $nameFirst; ?>"
+                   class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white <?php echo (empty($nameFirstError)) ? 'border-gray-200 focus:border-gray-500' : 'border-red-500'; ?>" >
+            <?php echo $nameFirstError; ?>
         </div >
 
         <div class="w-full md:w-1/2 px-3" >
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="name-last" >
-                Last Name
-            </label >
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                   id="name-last" name="name-last" type="text" placeholder="Doe" >
+            <label for="name-last" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >Last Name</label >
+            <input type="text" id="name-last" name="name-last" value="<?php echo $nameLast; ?>"
+                   class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white <?php echo (empty($nameLastError)) ? 'border-gray-200 focus:border-gray-500' : 'border-red-500'; ?>" >
+            <?php echo $nameLastError; ?>
         </div >
 
     </div >
 
     <div class="flex flex-wrap -mx-3 mb-6" >
         <div class="w-full px-3" >
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password" >
-                Email
-            </label >
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                   id="grid-email" type="email" name="email" placeholder="name@domain.com" >
-            <p class="text-gray-600 text-xs italic" >Make it as long and as crazy as you'd like</p >
+            <label for="email" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" > Email </label >
+            <input type="email" id="email" name="email" value="<?php echo $email; ?>"
+                   class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white <?php echo (empty($emailError)) ? 'border-gray-200 focus:border-gray-500' : 'border-red-500'; ?>" >
+            <?php echo $emailError; ?>
         </div >
     </div >
 
     <div class="flex flex-wrap -mx-3 mb-6" >
 
         <div class="w-full md:w-2/3 px-3 mb-6 md:mb-0" >
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="address-street" >Street</label >
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="address-street"
-                   name="address-street"
-                   type="text" placeholder="" >
-            <p class="text-red-500 text-xs italic" >Please fill out this field.</p >
+            <label for="address-street" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >Street</label >
+            <input type="text" id="address-street" name="address-street" value="<?php echo $addressStreet; ?>"
+                   class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white <?php echo (empty($addressStreetError)) ? 'border-gray-200 focus:border-gray-500' : 'border-red-500'; ?>" >
+            <?php echo $addressStreetError; ?>
         </div >
 
         <div class="w-full md:w-1/3 px-3" >
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="address-number" >Number</label >
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                   id="address-number" name="address-number" type="number" placeholder="" >
-            <p class="text-red-500 text-xs italic" >Please fill out this field.</p >
+            <label for="address-number" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >Number</label >
+            <input type="number" id="address-number" name="address-number" value="<?php echo $addressNumber; ?>"
+                   class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white <?php echo (empty($addressNumberError)) ? 'border-gray-200 focus:border-gray-500' : 'border-red-500'; ?>" >
+            <?php echo $addressNumberError; ?>
         </div >
 
     </div >
@@ -52,27 +48,28 @@
     <div class="flex flex-wrap -mx-3 mb-6" >
 
         <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0" >
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="address-zip" >Zip</label >
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="address-zip"
-                   name="address-zip"
-                   type="text" placeholder="" >
-            <p class="text-red-500 text-xs italic" >Please fill out this field.</p >
+            <label for="address-zip" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >Zip</label >
+            <input type="text" id="address-zip" name="address-zip" value="<?php echo $addressZip; ?>"
+                   class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white <?php echo (empty($addressZipError)) ? 'border-gray-200 focus:border-gray-500' : 'border-red-500'; ?>" >
+            <?php echo $addressZipError; ?>
         </div >
 
         <div class="w-full md:w-2/3 px-3" >
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="address-city" >City</label >
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                   id="address-city" name="address-city" type="text" placeholder="" >
-            <p class="text-red-500 text-xs italic" >Please fill out this field.</p >
+            <label for="address-city" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >City</label >
+            <input type="text" id="address-city" name="address-city" value="<?php echo $addressCity; ?>"
+                   class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white <?php echo (empty($addressCityError)) ? 'border-gray-200 focus:border-gray-500' : 'border-red-500'; ?>" >
+            <?php echo $addressCityError; ?>
         </div >
 
     </div >
 
     <div class="flex flex-wrap -mx-3 mb-2" >
+
         <div class="w-full px-3 text-center" >
-            <button id="submit" type="submit" name="submit" class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" >Order
+            <button type="submit" id="submit" name="submit" class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" >Order
             </button >
         </div >
+
     </div >
 
 </form >
--- index.php
+++ index.php
@@ -3,25 +3,113 @@
 declare(strict_types=1);
 
 session_start();
+var_dump($_SESSION);
+
+$totalValue = 0;
+
+$nameFirst = $nameLast = "";
+
+$email         = (isset($_SESSION["email"])) ? $_SESSION['email'] : '';
+$addressStreet = (isset($_SESSION["address-street"])) ? $_SESSION['address-street'] : '';
+$addressNumber = (isset($_SESSION["address-number"])) ? $_SESSION['address-number'] : '';
+$addressZip    = (isset($_SESSION["address-zip"])) ? $_SESSION['address-zip'] : '';
+$addressCity   = (isset($_SESSION["address-city"])) ? $_SESSION['address-city'] : '';
+
+$nameFirstError = $nameLastError = $emailError = $addressStreetError = $addressNumberError = $addressZipError = $addressCityError = "";
+
+$errorPrefix       = '<p class="text-red-500 text-xs italic" >';
+$errorSuffix       = '</p >';
+$errorRequiredText = 'Please fill out this field.';
+$errorMailText     = 'Wrong mail format.';
+$isFormValid       = true;
 
 // TODO: make validation function
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $nameFirst = isset($_POST['name-first']) ? $_POST['name-first'] : '';
 
-//if (!empty($_POST)) {
-    print_r($_POST);
+    function sanitizeData($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    if (!empty($_POST['name-first'])) {
+        $nameFirst = sanitizeData($_POST['name-first']);
+    } else {
+        $isFormValid    = false;
+        $nameFirstError = $errorPrefix . $errorRequiredText . $errorSuffix;
+    }
+
+    if (!empty($_POST['name-last'])) {
+        $nameLast = sanitizeData($_POST['name-last']);
+    } else {
+        $isFormValid   = false;
+        $nameLastError = $errorPrefix . $errorRequiredText . $errorSuffix;
+    }
+
 
     if (!empty($_POST['email'])) {
-        echo 'valid';
-//        if ($emailFormat) {
-//            echo 'email format ok';
-//        } else {
-//            echo 'mail format not ok';
-//        }
+        // CHECK IF EMAIL IS VALID
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailError  = $errorPrefix . $errorMailText . $errorSuffix;
+        }
+        else {
+            $email = $_POST['email'];
+        }
     } else {
-        // TODO: show error
-        echo 'not valid';
+        $isFormValid = false;
+        $emailError  = $errorPrefix . $errorRequiredText . $errorSuffix;
+    }
+
+
+    if (!empty($_POST['address-street'])) {
+        $addressStreet = sanitizeData($_POST['address-street']);
+    } else {
+        $isFormValid        = false;
+        $addressStreetError = $errorPrefix . $errorRequiredText . $errorSuffix;
+    }
+
+    if (!empty($_POST['address-number'])) {
+        $addressNumber = sanitizeData($_POST['address-number']);
+    } else {
+        $isFormValid        = false;
+        $addressNumberError = $errorPrefix . $errorRequiredText . $errorSuffix;
+    }
+
+    if (!empty($_POST['address-zip'])) {
+        $addressZip = sanitizeData($_POST['address-zip']);
+    } else {
+        $isFormValid     = false;
+        $addressZipError = $errorPrefix . $errorRequiredText . $errorSuffix;
+    }
+
+    if (!empty($_POST['address-city'])) {
+        $addressCity = sanitizeData($_POST['address-city']);
+    } else {
+        $isFormValid      = false;
+        $addressCityError = $errorPrefix . $errorRequiredText . $errorSuffix;
+    }
+
+    if ($isFormValid) {
+        echo 'Name first: ' . $nameFirst . '<br />';
+        echo 'Name last: ' . $nameLast . '<br />';
+        echo 'Email: ' . $email . '<br />';
+        echo 'Address street: ' . $addressStreet . '<br />';
+        echo 'Address number: ' . $addressNumber . '<br />';
+        echo 'Address zip: ' . $addressZip . '<br />';
+        echo 'Address city: ' . $addressCity . '<br />';
+
+        // ADD FIELDS TO SESSION
+        $_SESSION["email"]          = $email;
+        $_SESSION["address-street"] = $addressStreet;
+        $_SESSION["address-number"] = $addressNumber;
+        $_SESSION["address-zip"]    = $addressZip;
+        $_SESSION["address-city"]   = $addressCity;
+
+        // RESET FORM FIELDS
+        $nameFirst = $nameLast = $email = $addressStreet = $addressNumber = $addressZip = $addressCity = "";
     }
 
 }
@@ -31,6 +119,5 @@ $products = [
     ],
 ];
 
-$totalValue = 0;
 
 require('includes/view_form.php');
